9310bee75f13f35ca29908d0d67a496ab063743d
Eddy Wei <anlowee@outlook.com>
2019-06-26 14:12:02 +0800

    connect selW&dispW

--- TankGame/selectlevelwindow.cpp
+++ TankGame/selectlevelwindow.cpp
@@ -1,11 +1,13 @@
 #include "selectlevelwindow.h"
 #include "ui_selectlevelwindow.h"
+#include "displaywindow.h"
 
 SelectLevelWindow::SelectLevelWindow(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::SelectLevelWindow)
 {
     ui->setupUi(this);
+    connect(this->ui->pushButton, SIGNAL(clicked()), this, SLOT(on_pushButton_clicked()));
 }
 
 SelectLevelWindow::~SelectLevelWindow()
@@ -13,7 +15,9 @@ SelectLevelWindow::~SelectLevelWindow()
     delete ui;
 }
 
-void SelectLevelWindow::on_SelectLevelWindow_customContextMenuRequested(const QPoint &pos)
-{
 
+void SelectLevelWindow::on_pushButton_clicked()
+{
+    DisplayWindow *new_w = new DisplayWindow;
+    new_w->show();
 }
